cb26661de976821a4eafe0581b49a029c6cee0ad
이상준 <jun927@igloosec.com>
2019-01-28 17:03:58 +0900

    커밋

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
+
+
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
--- src/index.js
+++ src/index.js
@@ -4,9 +4,29 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+
+const myElem = <p>하이하이</p>;
+const MyCom =()=> <p>허이허이</p>;
+
+class MyKlassCom extends  React.Component {
+    render() {
+      return (
+      <div>
+        <p>qweqwe</p>
+        <MyCom />
+        {MyCom()}
+      </div>);
+    }
+}
+
+console.log(<myKlassCom />);
+  console.log(myElem,MyCom(),<myCom />);
+ReactDOM.render(<MyKlassCom />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
