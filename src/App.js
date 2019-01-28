4f9eb655013107bfce072ff82996fca6abfbc75f
이상준 <jun927@igloosec.com>
2019-01-28 15:33:01 +0900

    123123123

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload.123123123
           </p>
           <a
             className="App-link"
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React111111111111111111111111111111111
           </a>
         </header>
       </div>
